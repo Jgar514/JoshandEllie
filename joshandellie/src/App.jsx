b4b80552d6e2c677a020519c762c1528f88e7d55
Joshua Garvey <j.garvey514@yahoo.com>
2023-12-14 21:25:41 -0500

    fixing sizing

--- joshandellie/src/App.jsx
+++ joshandellie/src/App.jsx
@@ -59,8 +59,8 @@ export default function App() {
 	};
 
 	return (
-		<div className="relative z-0 height-screen overflow-hidden ">
-			<section className="relative w-full h-screen mx-auto white">
+		<div className="relative z-0 h-full bg-yellow-100  ">
+			<section className="relative w-full h-full mx-auto bg-white white">
 				{/* menu.opened */}
 
 				{isMenuOpen && (
@@ -105,34 +105,32 @@ export default function App() {
 
 				{/* icon.end */}
 
-				<div className="absolute top-2">
-					{/* nametag.line */}
-					<div className="absolute top-4 md:top-10 pl-6 ">
-						<div className="flex flex-col  items-center absolute top-0  ">
-							<div className="w-5 h-5 rounded-full black-gradient" />
-							<div className="w-1 sm:h-80 h-28 black-gradient" />
-						</div>
-
-						{/* nametag.name */}
-						<div className="pl-6 absolute top-2 left-8">
-							<h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
-								Joshua
-								<br />
-								Garvey
-							</h1>
-						</div>
+				{/* nametag.line */}
+				<div className="absolute top-4 md:top-10 pl-6 ">
+					<div className="flex flex-col  items-center absolute top-0  ">
+						<div className="w-5 h-5 rounded-full black-gradient" />
+						<div className="w-1 sm:h-80 h-28 black-gradient" />
 					</div>
 
-					{/* .com */}
-					<div className={"fixed bottom-20 w-full h-[100px] flex justify-end items-center pr-5 lg:mr-14   md:mr-10"}>
-						<h1 className="text-4xl md:text-6xl  lg:text-7xl text-black-gradient font-bold lg:pr-12">.com</h1>
+					{/* nametag.name */}
+					<div className="pl-6 absolute top-2 left-8">
+						<h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
+							Joshua
+							<br />
+							Garvey
+						</h1>
 					</div>
 				</div>
 
+				{/* .com */}
+				<div className={"absolute bottom-40 w-full h-[100px] flex justify-end items-center pr-5 lg:mr-14   md:mr-10 overflow-hidden"}>
+					<h1 className="text-4xl md:text-6xl  lg:text-7xl text-black-gradient font-bold lg:pr-12 ">.com</h1>
+				</div>
+
 				{/* SLIDER Animation*/}
-				<div className="absolute bottom-20 md:bottom-0 w-full    h-fit flex  flex-col gap-8 md:gap-2 text-lg justify-center items-center mb-[-30px] lg:mb-[-54px]">
-					<div className="text-3xl lg:text-4xl font-bold font-custom py-2 lg:pt-8 pt-4 ">Scroll</div>
-					<div className="w-[70px] h-[110px] rounded-3xl border-4 border-black  border-double flex justify-center items-start p-2 ">
+				<div className=" absolute bottom-0   md:bottom-0 w-full    h-fit flex  flex-col  md:gap-2 text-lg justify-center items-center ">
+					<div className="text-3xl lg:text-4xl font-bold font-custom  ">Scroll</div>
+					<div className=" w-[70px] h-[110px]  rounded-3xl border-4 border-black  border-double flex justify-center items-start p-2 ">
 						<motion.div
 							animate={{
 								y: [0, 30, 0],
@@ -149,117 +147,115 @@ export default function App() {
 				{/* END SLIDER Animation  */}
 
 				{/* Canvas and GLTF models */}
-				<div className="flex items-justify- h-full ">
-					<div className="w-full  absolute top-0 h-full   z-20 border-black">
-						<Canvas
-							gl={{ preserveDrawingBuffer: true }}
-							style={{ width: "100%", height: "100%" }}
-							onCreated={({ gl }) => {
-								// Additional setup if needed
-							}}
-						>
-							<Suspense fallback={<CanvasLoader />}>
-								<ScrollControls pages={15}>
-									<SheetProvider sheet={sheet}>
-										<Scene />
 
-										<Gltf src="./models/scene4.glb" />
-										<Gltf src="./models/rtb.glb" castShadow receiveShadow onClick={(e) => window.open("https://g.co/kgs/AW1mJL")} />
-										<Gltf src="./models/resume.glb" castShadow receiveShadow onClick={handleResumeClick} />
+				<div className="w-full  absolute top-0 h-full   z-20 border-black">
+					<Canvas
+						gl={{ preserveDrawingBuffer: true }}
+						style={{ width: "100%", height: "100%" }}
+						onCreated={({ gl }) => {
+							// Additional setup if needed
+						}}
+					>
+						<Suspense fallback={<CanvasLoader />}>
+							<ScrollControls pages={15}>
+								<SheetProvider sheet={sheet}>
+									<Scene />
 
-										{/* scene.ellie */}
+									<Gltf src="./models/scene4.glb" />
+									<Gltf src="./models/rtb.glb" castShadow receiveShadow onClick={(e) => window.open("https://g.co/kgs/AW1mJL")} />
+									<Gltf src="./models/resume.glb" castShadow receiveShadow onClick={handleResumeClick} />
 
-										<Gltf src="./models/ellienolabel3.glb" castShadow receiveShadow onClick={() => openCarousel(0)} />
+									{/* scene.ellie */}
 
-										{/* scene.contactcard */}
-										<Gltf src="./models/who3.glb" castShadow receiveShadow onClick={() => setSideBar((sideBar) => !sideBar)} />
-										{/* scene.elephant*/}
-										<Gltf src="./models/wood2.glb" castShadow receiveShadow />
-										{/* scene.computer */}
-										<Gltf src="./models/projects.glb" castShadow receiveShadow />
-										<Gltf src="./models/projectscreen.glb" castShadow receiveShadow onClick={redirect} />
-										{/* scene.socialicons */}
-										<Float
-											speed={8} // Animation speed, defaults to 1
-											rotationIntensity={0} // XYZ rotation intensity, defaults to 1
-											floatIntensity={0.5} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-											floatingRange={[0, 0.08]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+									<Gltf src="./models/ellienolabel3.glb" castShadow receiveShadow onClick={() => openCarousel(0)} />
+
+									{/* scene.contactcard */}
+									<Gltf src="./models/who3.glb" castShadow receiveShadow onClick={() => setSideBar((sideBar) => !sideBar)} />
+									{/* scene.elephant*/}
+									<Gltf src="./models/wood2.glb" castShadow receiveShadow />
+									{/* scene.computer */}
+									<Gltf src="./models/projects.glb" castShadow receiveShadow />
+									<Gltf src="./models/projectscreen.glb" castShadow receiveShadow onClick={redirect} />
+									{/* scene.socialicons */}
+									<Float
+										speed={8} // Animation speed, defaults to 1
+										rotationIntensity={0} // XYZ rotation intensity, defaults to 1
+										floatIntensity={0.5} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+										floatingRange={[0, 0.08]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+									>
+										<Gltf src="./models/github1.glb" castShadow receiveShadow onClick={(e) => window.open("https://github.com/Jgar514")} />
+										<Gltf src="./models/linkedin.glb" castShadow receiveShadow onClick={(e) => window.open("https://www.linkedin.com/in/josh-garvey-05944825a/")} />
+										<Gltf src="./models/insta.glb" castShadow receiveShadow onClick={(e) => window.open("https://www.instagram.com/joshgarvey/")} />
+										<Gltf src="./models/email.glb" castShadow receiveShadow onClick={openEmailWindow} />
+									</Float>
+									<Billboard
+										follow={true}
+										lockX={false}
+										lockY={false}
+										lockZ={false}
+										position={[-5.5, 4.9, 0.4]} // Lock the rotation on the z axis (default=false)
+									>
+										<Text color={"Green"} fontSize={0.04}>
+											Resume
+										</Text>
+										<Circle
+											args={[0.02, 64]}
+											position={[-0.1, 0.01, 0]} // Adjust the position to place the circle below the text
+											// Rotate the circle to face upward
 										>
-											<Gltf src="./models/github1.glb" castShadow receiveShadow onClick={(e) => window.open("https://github.com/Jgar514")} />
-											<Gltf src="./models/linkedin.glb" castShadow receiveShadow onClick={(e) => window.open("https://www.linkedin.com/in/josh-garvey-05944825a/")} />
-											<Gltf src="./models/insta.glb" castShadow receiveShadow onClick={(e) => window.open("https://www.instagram.com/joshgarvey/")} />
-											<Gltf src="./models/email.glb" castShadow receiveShadow onClick={openEmailWindow} />
-										</Float>
-										<Billboard
-											follow={true}
-											lockX={false}
-											lockY={false}
-											lockZ={false}
-											position={[-5.5, 4.9, 0.4]} // Lock the rotation on the z axis (default=false)
-										>
-											<Text color={"Green"} fontSize={0.04}>
-												Resume
-											</Text>
-											<Circle
-												args={[0.02, 64]}
-												position={[-0.1, 0.01, 0]} // Adjust the position to place the circle below the text
-												// Rotate the circle to face upward
-											>
-												<meshBasicMaterial attach="material" color="green" />
-											</Circle>
-										</Billboard>
+											<meshBasicMaterial attach="material" color="green" />
+										</Circle>
+									</Billboard>
 
-										<Billboard
-											follow={true}
-											lockX={false}
-											lockY={false}
-											lockZ={false}
-											position={[3.3, 5.1, -2.4]} // Lock the rotation on the z axis (default=false)
+									<Billboard
+										follow={true}
+										lockX={false}
+										lockY={false}
+										lockZ={false}
+										position={[3.3, 5.1, -2.4]} // Lock the rotation on the z axis (default=false)
+									>
+										<Text
+											font={Reboto}
+											color="white"
+											fontSize={0.4}
+											outlineWidth={0.02} // Adjust the width of the outline
+											outlineColor="black"
 										>
-											<Text
-												font={Reboto}
-												color="white"
-												fontSize={0.4}
-												outlineWidth={0.02} // Adjust the width of the outline
-												outlineColor="black"
-											>
-												Ellie
-											</Text>
-										</Billboard>
-										<Billboard
-											follow={true}
-											lockX={false}
-											lockY={false}
-											lockZ={false}
-											position={[3.3, 4.8, -2.4]} // Lock the rotation on the z axis (default=false)
+											Ellie
+										</Text>
+									</Billboard>
+									<Billboard
+										follow={true}
+										lockX={false}
+										lockY={false}
+										lockZ={false}
+										position={[3.3, 4.8, -2.4]} // Lock the rotation on the z axis (default=false)
+									>
+										<Text
+											font={Reboto}
+											color="red"
+											fontSize={0.2}
+											outlineWidth={0.01} // Adjust the width of the outline
+											outlineColor="black"
 										>
-											<Text
-												font={Reboto}
-												color="red"
-												fontSize={0.2}
-												outlineWidth={0.01} // Adjust the width of the outline
-												outlineColor="black"
-											>
-												//clickable
-											</Text>
-										</Billboard>
-										{/* scene.end */}
-									</SheetProvider>
-								</ScrollControls>
-							</Suspense>
-							<Preload all />
-						</Canvas>
-					</div>
+											//clickable
+										</Text>
+									</Billboard>
+									{/* scene.end */}
+								</SheetProvider>
+							</ScrollControls>
+						</Suspense>
+						<Preload all />
+					</Canvas>
 				</div>
 			</section>
 
 			<Sidebar className="z-50" {...{ sideBar, setSideBar }} />
-			<div className="container p-4">
-				{/* No image thumbnails in this version */}
 
-				<ImageCarousel isOpen={isCarouselOpen} closeCarousel={closeCarousel} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
-				<Modal isOpen={showResumeModal} onClose={() => setShowResumeModal(false)} />
-			</div>
+			{/* No image thumbnails in this version */}
+
+			<ImageCarousel isOpen={isCarouselOpen} closeCarousel={closeCarousel} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
+			<Modal isOpen={showResumeModal} onClose={() => setShowResumeModal(false)} />
 		</div>
 	);
 }

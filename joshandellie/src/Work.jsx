02eecb55e07385d140f80097728e2a925b797e98
jgar514 <j.garvey514@yahoo.com>
2024-06-17 00:56:46 -0400

    redirects file to root

--- joshandellie/src/Work.jsx
+++ joshandellie/src/Work.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import blackline from "./assets/work/homie.png"
+import jg from "./assets/work/joshuagarveydotcom.png"
+import ui from "./assets/work/mobileui.png"
 
 function Work() {
   // Example project data
   const projects = [
     {
       title: 'Project 1',
-      description: 'Description of project 1.',
+      description: 'Firebase for hosting, database, and serverless functions. Stripe payment api and name.com domain search api.',
       imageUrl: blackline,
       demoUrl: '#',
       githubUrl: '#'
@@ -15,14 +17,14 @@ function Work() {
     {
       title: 'Project 2',
       description: 'Description of project 2.',
-      imageUrl: 'https://placehold.it/300x200',
-      demoUrl: '#',
+      imageUrl: jg,
+      demoUrl: jg,
       githubUrl: '#'
     },
     {
       title: 'Project 3',
       description: 'Description of project 3.',
-      imageUrl: 'https://placehold.it/300x200',
+      imageUrl: ui,
       demoUrl: '#',
       githubUrl: '#'
     },
